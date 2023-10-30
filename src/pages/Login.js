cc5c980793f246b014525213dba963ec7f7e6c75
Adan Garcia Leyva <adan.garley@gmail.com>
2023-10-30 15:22:34 -0600

    n

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,6 @@
 import { NavBar, Footer } from "./PageComponents";
 import { SignIn } from "../components/Auth";
-// import "../css/style.css";
 import LoginForm from "../components/LoginForm";
-// import '../css/login.css'
 import '../css/global-for-pages.css'
 
 
@@ -10,10 +8,6 @@ function Login() {
     return (
         <div>
             <NavBar />
-
-            {/* <div className="main-container-login">
-                <LoginForm />
-            </div> */}
             
             <div className="flex-container">
                 <LoginForm />
