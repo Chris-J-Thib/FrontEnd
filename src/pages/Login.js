b7e60e630ebacf95e87ce398c058cf00f1709cb5
Adan Garcia Leyva <adan.garley@gmail.com>
2023-10-30 15:20:36 -0600

    flex-container

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,24 @@
 import { NavBar, Footer } from "./PageComponents";
 import { SignIn } from "../components/Auth";
-import "../css/style.css";
+// import "../css/style.css";
 import LoginForm from "../components/LoginForm";
-import '../css/login.css'
+// import '../css/login.css'
+import '../css/global-for-pages.css'
 
 
 function Login() {
     return (
         <div>
             <NavBar />
-            {/* <div>
-                <ul>
-                    <li><input id="Username" placeholder="Username" type="text"></input></li>
-                    <li><input id="Password" placeholder="Password" type="password"></input></li>
-                </ul>
-            </div>
-            <div>
-                <button id="butLog" onClick={() =>
-                     SignIn(document.getElementById("Username").value,document.getElementById("Password").value)}
-                     >Login</button><hr />
-                
-                <label id="lblCheck" for="butSign">Don't have an account yet?</label>
-                <button id="butSign"><a href="/SignUp">SignUp</a></button>
+
+            {/* <div className="main-container-login">
+                <LoginForm />
             </div> */}
-            <div className="main-container-login">
+            
+            <div className="flex-container">
                 <LoginForm />
             </div>
             
-
             <Footer />
         </div>
     )
