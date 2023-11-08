93126d9d1ce2c50cd298671ed75a76cd48eef1ab
Chris <chris.j.thib@gmail.com>
2023-11-08 15:44:47 -0700

    messages field now in LS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import ReactDOM from 'react-dom/client';
 import users from './databases/users.json';
+import msgData from './databases/messages.json';
 import App from './App';
 import { SetUsers } from './components/DataManagement';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-if(localStorage.getItem("users")==null){
+if(localStorage.getItem("users") == null){
     SetUsers(users);
+}
+
+if(localStorage.getItem("msgs") == null){
+    var dict = new Object();
+    msgData.forEach(m=>{
+        dict[m.messageId] = m;
+    })
+    localStorage.setItem("msgs",JSON.stringify(dict));
 }
 
 
