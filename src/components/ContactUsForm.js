12c24bf1f80e55131f704c8c7d1484f659b95849
Adan Garcia Leyva <adan.garley@gmail.com>
2023-11-08 10:54:25 -0700

    contact us retrieve user info

--- src/components/ContactUsForm.js
+++ src/components/ContactUsForm.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { useState } from "react";
 import '../css/contactUsForm.css';
 
 function ContactUsForm() {
+  //user logged in
+  let userId = sessionStorage.getItem("id");
+  let userFullName = '';
+  let userEmail = '';
+  let userPhone = '';
+  let userDepartment = '';
+  let userProgram = '';
+
+    //retrieve user info from local storage
+    for(let i = 0; i < localStorage.length; i++){
+      let userOnDDBB = JSON.parse(localStorage.getItem(i));
+          if(userOnDDBB.id == userId){
+            userFullName = userOnDDBB.firstName + " " + userOnDDBB.lastName;
+            userEmail = userOnDDBB.email;
+            userPhone = userOnDDBB.phone;
+            userDepartment = userOnDDBB.department;
+            userProgram = userOnDDBB.program;
+          } 
+    }
+
+  const [message, setMessage] = useState({ userFullName:userFullName, userEmail:userEmail, userPhone:userPhone, userDepartment:userDepartment, userProgram:userProgram, question:"" });
+
+  const handleNameChange = (event) => {
+    setMessage({...message, userFullName:event.target.value});
+  };
+
+  const handleEmailChange = (event) => {
+    setMessage({...message, userEmail:event.target.value});
+  };
+
+  const handlePhoneChange = (event) => {
+    setMessage({...message, userPhone:event.target.value});
+  };
+
+  const handleDepartmentChange = (event) => {
+    setMessage({...message, userDepartment:event.target.value});
+  };
+
+  const handleProgramChange = (event) => {
+    setMessage({...message, userProgram:event.target.value});
+  };
+
+  const handleQuestionChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setMessage({...message, [name]:value});
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Your message has been sent!');
+    console.log(message);
+  };
+
   return (
     <div className="question-form-container">
 
@@ -9,42 +64,40 @@ function ContactUsForm() {
         <h2>Contact admin</h2>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='logged-in-user-info'>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" />
-          <br />
+          <input type="text" id="name" value={message.userFullName} onChange={handleNameChange} disabled={sessionStorage.getItem("id") != null} /><br />
+
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
-          <br />
+          <input type="email" id="email" value={message.userEmail} onChange={handleEmailChange} disabled={sessionStorage.getItem("id") != null} /><br />
+
           <label htmlFor="phone">Phone:</label>
-          <input type="tel" id="phone" />
-          <br />
-          <select name="Department" id="dpt">
-            <option value="" disabled selected hidden>Department</option>
+          <input type="tel" id="phone" value={message.userPhone} onChange={handlePhoneChange} disabled={sessionStorage.getItem("id") != null} /><br />
+
+          <select name="Department" id="dpt" value={message.userDepartment != null ? message.userDepartment: ""} onChange={handleDepartmentChange} disabled={sessionStorage.getItem("id") != null} >
+            <option value="" disabled hidden>Department</option>
             <option value="SD">Software Development</option>
-          </select>
-          <br />
-          <select name="Program" id="prgm">
-            <option value="" disabled selected hidden>Program</option>
+          </select><br />
+
+          <select name="Program" id="prgm" value={message.userProgram != null ? message.userProgram : ""} onChange={handleProgramChange} disabled={sessionStorage.getItem("id") != null} >
+            <option value="" disabled hidden>Program</option>
             <option>Certificate</option>
             <option>Diploma</option>
             <option>Post-Diploma</option>
-          </select>
-          <br />
+          </select><br />
         </div>
 
 
         <div className="question-section">
-          <label htmlFor="question">Question:</label>
-          <br />
-          <textarea id="question" />
-          <br />
+          <label htmlFor="question">Question:</label><br />
+
+          <textarea id="question" onChange={handleQuestionChange} name='question' value={message.question} /><br />
+
         </div>
 
         <div className="buttons-section">
           <button type="submit">Submit</button>
-          <button type="reset">Reset</button>
         </div>
 
       </form>
