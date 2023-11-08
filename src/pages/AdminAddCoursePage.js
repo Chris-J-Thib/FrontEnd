52e7b11feac97edbf9fbf2bad36ae804e229cc97
Javier Saez <jgsaez@uc.cl>
2023-11-08 12:42:11 -0700

    minor changes

--- src/pages/AdminAddCoursePage.js
+++ src/pages/AdminAddCoursePage.js
@@ -1,10 +1,11 @@
 import { Footer, NavBar } from "../components/PageComponents";
 import AdminAddCourses from "../components/AdminAddCourses";
 import "../css/global-for-pages.css";
+import "../css/contactUsForm.css";
 
 function CourseAddition() {
     return (
-        <div className="AdminAddCoursePage">
+        <div>
             <NavBar />
             <div className="flex-container">
                 <AdminAddCourses />
